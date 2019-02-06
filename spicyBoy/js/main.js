58ff193a7098eb84b4e77cc25b815394ff1a12c4
ursulacj <ursulacedillojohnson@gmail.com>
2019-02-05 23:05:02 -0600

    added Reset button

--- spicyBoy/js/main.js
+++ spicyBoy/js/main.js
@@ -1,46 +1,23 @@
 /*----- constants -----*/ 
 const canvas = document.getElementById('canvas');
-    canvas.width = window.innerWidth;
+    canvas.width = window.innerWidth * .85;
     canvas.height = window.innerHeight;
 
 const c = canvas.getContext('2d');
 
 /*----- app's state (variables) -----*/ 
 
-let x = innerWidth / 2;
+let x = canvas.width / 2;
 let y = innerHeight - 50;
 let radius = 30;
-let dx = 10;
+let dx = 25;
+
 let color = randomRGBA();
 
-function drawCircle() {
-    c.clearRect(0, 0, innerWidth, innerHeight);
-
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = color;
-    c.fillStyle = color;
-    c.fill();
-
-    if (canvas.width < 500) {
-        radius = 20;
-    } else {
-        radius = 30;
-    }
-};
-
-function drawTaco() {
-    c.fillRect(400, 15, 55, 135);
-    c.fillStyle = "red";
-};
-
-
-
-function drawWork() {
-    c.fillRect(250, 20, 50, 100);
-    c.fillStyle = 'white';
-    c.fill();
-}
+var rx = Math.round(Math.random() * innerWidth);
+var ry = 20;
+let rdy = Math.random() + .0001;
+var rWidth = 55;
 
 /*----- cached element references -----*/ 
 
@@ -49,10 +26,10 @@ function drawWork() {
 /*----- event listeners -----*/ 
 
 window.addEventListener('resize', function() {
-   canvas.width = this.innerWidth;
+   canvas.width = this.innerWidth * .85;
    canvas.height = this.innerHeight;
 
-   x = this.innerWidth / 2;
+   x = canvas.width / 2;
    y = this.innerHeight - 50;
 
     if (canvas.width < 500 || canvas.height < 150) {
@@ -62,14 +39,51 @@ window.addEventListener('resize', function() {
 })
 
 window.addEventListener('keydown', arrowKey, true);
-window.setInterval(init, 10);
+window.setInterval(init, 20);
 
 /*----- functions -----*/
 
+function spicyBoy() {
+   
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
+    c.fillStyle = 'white';
+    c.fill();
+
+    if (canvas.width < 500 || canvas.height < 500) {
+        radius = 20;
+    } else {
+        radius = 30;
+    }
+
+    taco();
+};
+
+
+function taco() {
+
+    requestAnimationFrame(taco);
+
+    c.beginPath();
+    c.fillRect(rx, ry, rWidth, 135);
+    c.fillStyle = color;
+    
+    ry += rdy;
+
+    if(ry > canvas.height) {
+        return;
+    }
+};
+
+function work() {
+    c.fillRect(250, 20, 50, 80);
+    c.fillStyle = color; 
+}
+
 function init() {
-    drawCircle();
-    drawTaco();
-    drawWork(); 
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    spicyBoy();
+    work(); 
 }
 
 function randomRGBA() {
@@ -87,6 +101,4 @@ function arrowKey(event) {
             break;
     };
 };
-
-
 
